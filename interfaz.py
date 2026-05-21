7f3c52c1efe279145ee264faed3776b20384b659
Angel-apc10 <angelcantun02@gmail.com>
2026-05-21 10:08:41 -0600

    fix(interfaz.py): Cambiar creacion de widgets en la pantalla.  Modificar las funciones de la interfaz y la salida en pantalla

--- interfaz.py
+++ interfaz.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+
 import ttkbootstrap as ttk
 import operadores_ed as op
 
@@ -6,9 +6,10 @@ class app():
     def __init__(self):
         self.root = ttk.Window(themename="cerculean")
         self.root.title("Calculadora de EDS")
-        self.root.geometry("400x400")
+        self.root.geometry("600x400")
         
     def crear_widgets(self):
+        
         self.label = ttk.Label(self.root, text= "Calculadora de Ecuaciones Diferenciales :D")
         self.label.pack(pady=20)
         self.label.config(font=("display", 10, "bold"), background="#1f77b4", foreground="white", padding=10)
@@ -20,35 +21,33 @@ class app():
         self.text_box = ttk.Entry(self.root, width = 25)
         self.text_box.pack(pady=10)
         
-    def create_button(self):
-        self.button = ttk.Button(self.root, text="Calcular", command=self.realizar_calculo)
+        self.button = ttk.Button(self.root, text="Calcular", command=self.realizar_calculo, style = "success")
         self.button.pack(pady=10)
-    
+        
         #Control de estado del botón (habilitado/deshabilitado)
         self.button.config(state="disabled")
         self.text_box.bind("<KeyRelease>", self.controlar_estado_boton)
         
-    #Frame para mostrar propiedades de la ecuación diferencial 
-    def create_frame(self):
-        self.frame = ttk.Frame(self.root, padding = 12)
+        self.frame = ttk.Frame(self.root, padding = 12, relief= "flat", style = "secondary")
         self.frame.pack(pady=10)
-
+             
     def controlar_estado_boton(self, event):
         if self.text_box.get().strip():
             self.button.config(state="normal")
         else:
             self.button.config(state="disabled")
-            
+    
     def realizar_calculo(self):
         ed_input = self.text_box.get()
         resultado = op.resolver_edo(ed_input)
-        re_label = ttk.Label(self.frame, text=f"Solución: {resultado}")
+        res_text = str(resultado.rhs).replace("**", "^")
+        
+        re_label = ttk.Label(self.frame, text=f"Solución: y = {res_text}")
         re_label.pack(pady=10)
-
+        re_label.config(foreground= "#0f4bd6", font = ("bold"))
+        
 if __name__ == "__main__":
     my_app = app()
     my_app.crear_widgets()
-    my_app.create_frame()
-    my_app.create_button()
     my_app.root.mainloop()
         
--- operadores_ed.py
+++ operadores_ed.py
@@ -15,7 +15,8 @@ print("Solución Normal:", sol_normal)
 def resolver_edo(eq_input):
     #eq_input = input("Ingrese la ecuación diferencial (ejemplo: y' + 2*y = exp(x)): ")
     sol_normal = sympy.dsolve(interpretar_edo(eq_input), y)
-    print("Solución:", sol_normal)
+    #print("Solución general de la ecuación diferencial: ", sol_normal)
+    #print("y = ", sol_normal.rhs)
     return sol_normal
     
 def interpretar_edo(eq_input):
@@ -29,8 +30,16 @@ def obtener_propiedades_edo(eq_input):
     eq = interpretar_edo(eq_input)
     orden = sympy.ode_order(eq, y)
     es_lineal = sympy.checkodesol(eq, sympy.dsolve(eq, y))
+    
+    print("Propiedades de la ecuación diferencial")
+    print(f"Orden: {orden} ")
+    
+    if es_lineal == True:
+        print("La ecuación es lineal.")
+        
     return orden, es_lineal
 
+#resolver_edo("y' = 5*x")
 # Para obtener propiedades de la ecuación diferencial como orden, linealidad:
 #orden = sympy.ode_order(eq_homogenea, y)
 
